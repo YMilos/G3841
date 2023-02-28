36546bc31aa6ac7609bf7fd27b862fed00742b52
Yegor <miloserdov92@gmail.com>
2023-02-28 19:02:30 +0300

    add solution for task 2, 4, 6, 8.

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Считываем данные с консоли для первого числа
+string? inputNum1 = Console.ReadLine();
+// Считываем данные с консоли для второго числа
+string? inputNum2 = Console.ReadLine();
+// Проверяем, чтобы данные не были пустыми
+if (inputNum1 != null && inputNum2 != null)
+{
+    // Парсим введенное число
+    int number1 = int.Parse(inputNum1);
+    // Парсим введенное число
+    int number2 = int.Parse(inputNum2);
+    // Сравниваем числа
+    if (number1 > number2)
+    {
+        Console.WriteLine("Первое число больше второго");
+    }
+    else if (number1 == number2)
+    {
+        Console.WriteLine("Введенные числа равны");
+    }
+    else
+    {
+        Console.WriteLine("Второе число больше первого");
+    }
+
+}
---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Считываем данные с консоли для первого числа
+string? inputNum1 = Console.ReadLine();
+// Считываем данные с консоли для второго числа
+string? inputNum2 = Console.ReadLine();
+// Считываем данные с консоли для третьего числа
+string? inputNum3 = Console.ReadLine();
+// Проверяем, чтобы данные не были пустыми
+if (inputNum1 != null && inputNum2 != null && inputNum3 != null)
+{
+    // Парсим введенное число
+    int number1 = int.Parse(inputNum1);
+    // Парсим введенное число
+    int number2 = int.Parse(inputNum2);
+    // Парсим введенное число
+    int number3 = int.Parse(inputNum3);
+    // Сравниваем числа
+    if (number1 > number2)
+    {
+        if (number1 > number3)
+        {
+            Console.WriteLine(number1);
+        }
+        else
+        {
+            Console.WriteLine(number3);
+        }
+
+    }
+    else if (number2 > number3)
+    {
+        Console.WriteLine(number2);
+
+    }
+    else
+    {
+        Console.WriteLine(number3);
+    }
+}
