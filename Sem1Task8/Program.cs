705d8183a7e10e6d5c864a99dfccd670c822679d
Yegor <miloserdov92@gmail.com>
2023-02-28 18:59:17 +0300

    add solution for task 2, 4, 6, 8

---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Считываем данные с консоли 
+string? inputNum1 = Console.ReadLine();
+
+// Проверяем, чтобы данные не были пустыми
+if (inputNum1 != null)
+{
+    // Парсим введенное число
+    int number1 = int.Parse(inputNum1);
+    // Добавляем пустую строку, для вывода четных чисел
+    string evenNumber = String.Empty;
+
+    for (int i = 2; i < number1; i = i + 2)
+    {
+        evenNumber = evenNumber + i + ", ";
+    }
+
+    if (number1 % 2 == 0)
+    {
+        evenNumber = evenNumber + number1;
+    }
+    else
+    {
+      evenNumber =  evenNumber.Remove(evenNumber.Length - 2);
+    }
+    Console.WriteLine(evenNumber);
+}
